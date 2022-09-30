260abd3380fa9cc4f922efe7780de5e518084a5d
George <georgehender997@gmail.com>
2022-09-29 22:40:02 -0400

    getting stuff down

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-let table = document.getElementById(Table)
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
 
+table.appendChild(thead);
+table.appendChild(tbody);
+
+document.getElementById('table').appendChild(table);
+
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "Sr. No.";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "Name";
+let heading_3 = document.createElement('th');
+heading_3.innerHTML = "Company";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+thead.appendChild(row_1);
