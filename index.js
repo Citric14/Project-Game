1435514e7687059c9905deda95875a5f22a2f192
George <georgehender997@gmail.com>
2022-09-30 21:53:01 -0400

    almost there

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 //select the character and block
-let character = document.getElementById("character")
+let character = document.getElementById("character");
+let block = document.getElementById("block");
 //allow the character to jump on click
-
+function jump() {
+    if(character.classList != "animation") {
+        character.classList.add("animation");
+    }
+    setTimeout(function(){
+        character.classList.remove("animation");
+    }, 500)
+}
 //set a collision
 
 //make a counter
+let counter = document.getElementById("counter")
 
+let count = 0;
+
+updateDisplay();
+
+document.addEventListener("click", () => {
+    count++;
+    updateDisplay();
+})
+
+function updateDisplay(){
+    counter.innerHTML = `Score: ${count}`;
+}
 //have a end message
 
 
