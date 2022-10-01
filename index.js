92830140a2d57ede0db76d01b1d8065c1f6d46c2
George <georgehender997@gmail.com>
2022-09-30 21:00:56 -0400

    changed the game but making progress

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-let table = document.createElement('table');
-let thead = document.createElement('thead');
-let tbody = document.createElement('tbody');
+//select the character and block
 
-table.appendChild(thead);
-table.appendChild(tbody);
+//allow the character to jump on click
 
-document.getElementById('table').appendChild(table);
+//set a collision
 
-let row_1 = document.createElement('tr');
-let heading_1 = document.createElement('th');
-heading_1.innerHTML = "Sr. No.";
-let heading_2 = document.createElement('th');
-heading_2.innerHTML = "Name";
-let heading_3 = document.createElement('th');
-heading_3.innerHTML = "Company";
+//make a counter
 
-row_1.appendChild(heading_1);
-row_1.appendChild(heading_2);
-row_1.appendChild(heading_3);
-thead.appendChild(row_1);
+//have a end message
 
 
